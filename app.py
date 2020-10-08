0acc885ba2845f9ef75060cc92acda5db7854f1f
Nick Nasse <nasse@Nasses-Pro-Book.local>
2020-10-07 21:46:06 -0700

    flask app updated to read csv into MongoDB

--- app.py
+++ app.py
@@ -1,38 +1,45 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-
+import csv
 
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/mars"
-# mongo = PyMongo(app)
+mongo = PyMongo(app, uri="mongodb://localhost:27017/nyt")
 
 
 @app.route('/')
+def db():
+    data = []
+    with open("us-counties.csv") as file:
+        filereader = csv.DictReader(file)
+        for row in filereader:
+            data.append(row)
+
+    covidData = mongo.db.covid
+    covidData.insert_many(data)
+    return redirect("/home")
+
+@app.route('/home')
 def home():
-    # listings = mongo.db.listings
-    return render_template('home.html')
+    covidData = mongo.db.covid
+    return render_template('home.html', covidData=covidData)
 
 @app.route('/vis_1')
 def vis_1():
-    # listings = mongo.db.listings
-    return render_template('vis_1.html')
+    covidData = mongo.db.covid
+    return render_template('vis_1.html', covidData=covidData)
 
 @app.route('/vis_2')
 def vis_2():
-    # listings = mongo.db.listings
-    return render_template('vis_2.html')
+    covidData = mongo.db.covid
+    return render_template('vis_2.html', covidData=covidData)
 
 @app.route('/vis_3')
 def vis_3():
-    # listings = mongo.db.listings
-    return render_template('vis_3.html')
+    covidData = mongo.db.covid
+    return render_template('vis_3.html', covidData=covidData)
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-
-# def home():
-#     # listings = mongo.db.listings
-#     return render_template('home.html', listings=listings)
